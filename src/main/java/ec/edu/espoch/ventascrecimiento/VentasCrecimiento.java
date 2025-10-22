3af38534ed5c1ef53771e721323a5d8e0f6290bc
AdeJhayr <brandonjaramillo763>
2025-10-22 00:53:44 -0500

    proyecto integrador

--- src/main/java/ec/edu/espoch/ventascrecimiento/VentasCrecimiento.java
+++ src/main/java/ec/edu/espoch/ventascrecimiento/VentasCrecimiento.java
@@ -30,40 +30,7 @@ public class VentasCrecimiento
             ventasSemanales[i] = sumaSemana;
             System.out.println("Total de ventas en la semana "+(i+1)+":"+sumaSemana);
         }
-        double[] derivadas = new double[semanas - 1];
-        for (int i = 0; i < semanas - 1; i++) {
-            derivadas[i] = ventasSemanales[i + 1] - ventasSemanales[i]; // derivada semanal
-        }
-
-        // Mostrar resultados por semana
-        System.out.println("\n=== Comparación de ventas semana a semana ===");
-        for (int i = 0; i < derivadas.length; i++) {
-            System.out.print("De la semana " + (i + 1) + " a la semana " + (i + 2) + ": ");
-            if (derivadas[i] > 0) {
-                System.out.println("Crecimiento (" + derivadas[i] + ")");
-            } else if (derivadas[i] < 0) {
-                System.out.println("Decrecimiento (" + derivadas[i] + ")");
-            } else {
-                System.out.println("Estable (" + derivadas[i] + ")");
-            }
-        }
-
-        // Calcular promedio de crecimiento semanal
-        double sumaDerivadas = 0;
-        for (int i = 0; i < derivadas.length; i++) {
-            sumaDerivadas += derivadas[i];
-        }
-        double promedio = sumaDerivadas / derivadas.length;
-
-        System.out.println("\nPromedio de crecimiento semanal: " + promedio);
-        if (promedio > 0) {
-            System.out.println("Tendencia general: CRECIMIENTO 📈");
-        } else if (promedio < 0) {
-            System.out.println("Tendencia general: DECRECIMIENTO 📉");
-        } else {
-            System.out.println("Tendencia general: ESTABLE ➖");
-        }
+        
     }
-    
 }
 
